bfb1b89409a2b7d5828a2541fc4338aee4c66b6f
rohit2006ranjan <rohit2006ranjan@gmail.com>
2022-01-07 01:09:38 +0530

    First Commit

--- Models/StudentModel.go
+++ Models/StudentModel.go
@@ -1,12 +1,12 @@
 package Models
 
 type Student struct {
-	Student_id      uint   `json:"student_id" gorm:primaryKey`
-	First_name      string `json:"first_name"`
-	Last_name       string `json:"last_name"`
-	Mobile          string `json:"mobile" gorm:unique`
-	Year_of_joining string `json:"year_of_joining"`
-	Email_id        string `json:"email_id"`
+	Student_id      uint64 `gorm:"primary_key"`
+	First_name      string
+	Last_name       string
+	Mobile          string `gorm:"unique"`
+	Year_of_joining string
+	Email_id        string
 }
 
 func (b *Student) TableName() string {
