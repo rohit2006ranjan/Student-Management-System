f13367e808fddcf50fa1d3ff4475c22bf4ce3115
rohit2006ranjan <rohit2006ranjan@gmail.com>
2022-01-07 00:45:41 +0530

    First Commit

--- Models/StudentModel.go
+++ Models/StudentModel.go
@@ -4,7 +4,7 @@ type Student struct {
 	Student_id      uint   `json:"student_id" gorm:primaryKey`
 	First_name      string `json:"first_name"`
 	Last_name       string `json:"last_name"`
-	Mobile          string `json:"mobile" gorm:uniqueKey`
+	Mobile          string `json:"mobile" gorm:unique`
 	Year_of_joining string `json:"year_of_joining"`
 	Email_id        string `json:"email_id"`
 }
