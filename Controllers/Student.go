403fe370da31615a7ae786b0d43a606ddf26ae5c
rohit2006ranjan <rohit2006ranjan@gmail.com>
2022-01-06 16:58:28 +0530

    init 3 commit

--- Controllers/Student.go
+++ Controllers/Student.go
@@ -8,7 +8,7 @@ import (
 	"github.com/rohit2006ranjan/Student-Management-System/Models"
 )
 
-//GetUsers ... Get all users
+//GetStudents ... Get all students
 func GetStudents(c *gin.Context) {
 	var student []Models.Student
 	err := Models.GetAllStudents(&student)
@@ -19,11 +19,11 @@ func GetStudents(c *gin.Context) {
 	}
 }
 
-//CreateUser ... Create User
+//CreateStudent ... Create Student
 func CreateStudent(c *gin.Context) {
 	var student Models.Student
 	c.BindJSON(&student)
-	err := Models.CreateUser(&student)
+	err := Models.CreateStudent(&student)
 	if err != nil {
 		fmt.Println(err.Error())
 		c.AbortWithStatus(http.StatusNotFound)
@@ -32,7 +32,7 @@ func CreateStudent(c *gin.Context) {
 	}
 }
 
-//GetUserByID ... Get the user by id
+//GetStudentByID ... Get the student by id
 func GetStudentByID(c *gin.Context) {
 	id := c.Params.ByName("student_id")
 	var student Models.Student
@@ -44,31 +44,31 @@ func GetStudentByID(c *gin.Context) {
 	}
 }
 
-//UpdateUser ... Update the user information
-func UpdateUser(c *gin.Context) {
-	var user Models.User
-	id := c.Params.ByName("id")
-	err := Models.GetUserByID(&user, id)
+//UpdateStudent ... Update the student information
+func UpdateStudent(c *gin.Context) {
+	var student Models.Student
+	id := c.Params.ByName("student_id")
+	err := Models.GetStudentByID(&student, id)
 	if err != nil {
-		c.JSON(http.StatusNotFound, user)
+		c.JSON(http.StatusNotFound, student)
 	}
-	c.BindJSON(&user)
-	err = Models.UpdateUser(&user, id)
+	c.BindJSON(&student)
+	err = Models.UpdateStudent(&student, id)
 	if err != nil {
 		c.AbortWithStatus(http.StatusNotFound)
 	} else {
-		c.JSON(http.StatusOK, user)
+		c.JSON(http.StatusOK, student)
 	}
 }
 
-//DeleteUser ... Delete the user
-func DeleteUser(c *gin.Context) {
-	var user Models.User
-	id := c.Params.ByName("id")
-	err := Models.DeleteUser(&user, id)
+//DeleteStudent ... Delete the student
+func DeleteStudent(c *gin.Context) {
+	var student Models.Student
+	id := c.Params.ByName("student_id")
+	err := Models.DeleteStudent(&student, id)
 	if err != nil {
 		c.AbortWithStatus(http.StatusNotFound)
 	} else {
-		c.JSON(http.StatusOK, gin.H{"id" + id: "is deleted"})
+		c.JSON(http.StatusOK, gin.H{"student_id" + id: "is deleted"})
 	}
 }
